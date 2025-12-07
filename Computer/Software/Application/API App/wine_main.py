581d7893e2e9fa38c0e950afd641534f8cb72258
SOUROV <host.sourov@gmail.com>
2025-12-07 23:19:53 +0600

    Delete Computer/Software/Application/API App/wine_main.py

--- Computer/Software/Application/API App/wine_main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from GUI import WineLauncher
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    w = WineLauncher()
-    w.show()
-    sys.exit(app.exec_())
